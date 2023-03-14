f254c085c2450a6b9edbf287eb0e4e1884c1cdef
yusabolen <yusabugrabolen@gmail.com>
2023-03-14 18:24:11 +0100

    movie

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
-
+  //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
-   
+    //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
     extend: {
       colors: {
         "gray-dark-main": "#23242a",
@@ -19,3 +19,4 @@ module.exports = {
   darkMode: "class",
   plugins: [require("tw-elements/dist/plugin")],
 };
+
